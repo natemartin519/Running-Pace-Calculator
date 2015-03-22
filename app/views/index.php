b2c7ad8d873e324e8feb9c80e70f602b31244808
natemartin519 <natemartin519@gmail.com>
2015-03-22 15:15:31 -0400

    Added Bootstrap to front-end. Added a convert km/hour to mile/hour and a convert mile/hour to km/hour to front-end.

--- app/views/index.php
+++ app/views/index.php
@@ -2,22 +2,132 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
     <title>Pace Convert</title>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
-    <form action="/miles" method="POST">
-        <input type="hidden" name ="distance" value="1">
-        Min/Km: <input type="text" name="time">
-        <input type="submit" value="Convert to Min/Km">
-    </form>
 
-    <br>
+<div class="container">
 
-    <form action="/kilometres" method="POST">
-        <input type="hidden" name ="distance" value="1">
-        Min/Mile: <input type="text" name="time">
-        <input type="submit" value="Convert to Min/Mile">
-    </form>
+    <div class="page-header">
+        <h1>Pace Conversion Tool
+            <small>By Nathan Martin</small>
+        </h1>
+    </div>
+
+    <div class="row">
+
+        <div class="col-lg-3">
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Convert Min/Km to Min/Mile</div>
+                <div class="panel-body">
+
+                    <form action="/minutes/mile" method="POST">
+                        <input type="hidden" name="distance" value="1">
+
+                        <div class="input-group">
+                            <input class="form-control" type="text" id="time" name="time">
+
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Go!</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Convert Min/Mile to Min/Km</div>
+                <div class="panel-body">
+
+                    <form action="/minutes/kilometre" method="POST">
+                        <input type="hidden" name="distance" value="1">
+
+                        <div class="input-group">
+                            <input class="form-control" type="text" id="time" name="time">
+
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Go!</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Convert Km/Hour to Mile/Hour</div>
+                <div class="panel-body">
+
+                    <form action="/miles/hour" method="POST">
+                        <input type="hidden" name="time" value="1">
+
+                        <div class="input-group">
+                            <input class="form-control" type="text" id="distance" name="distance">
+
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Go!</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Convert Mile/Hour to Km/Hour</div>
+                <div class="panel-body">
+
+                    <form action="/kilometres/hour" method="POST">
+                        <input type="hidden" name="time" value="1">
+
+                        <div class="input-group">
+                            <input class="form-control" type="text" id="distance" name="distance">
+
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Go!</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 require 'vendor/autoload.php';
 
-use App\Models\ImperialPace;
+use App\ImperialPace;
+use App\MetricPace;
+use App\Models\Hours;
 use App\Models\Kilometres;
-use App\Models\MetricPace;
 use App\Models\Miles;
 use App\Models\Minutes;
 use Slim\Slim;
@@ -17,7 +18,7 @@ $app->get('/', function() use ($app) {
     $app->render('index.php');
 });
 
-$app->post('/miles', function() use ($app) {
+$app->post('/minutes/mile', function() use ($app) {
     $distance = new Kilometres($app->request->params('distance'));
     $time = new Minutes($app->request->params('time'));
     $pace = new ImperialPace($distance, $time);
@@ -25,12 +26,28 @@ $app->post('/miles', function() use ($app) {
     echo $time->minutes() . ' Min/Km is ' . round($pace->minutesMile(), 2) . ' Min/Mile';
 });
 
-$app->post('/kilometres', function() use ($app) {
+$app->post('/minutes/kilometre', function() use ($app) {
     $distance = new Miles($app->request->params('distance'));
     $time = new Minutes($app->request->params('time'));
     $pace = new MetricPace($distance, $time);
 
-    echo $time->minutes() . ' Min/Km is ' . round($pace->minutesKilometre(), 2) . ' Min/Mile';
+    echo $time->minutes() . ' Min/Mile is ' . round($pace->minutesKilometre(), 2) . ' Min/Km';
+});
+//////////////////
+$app->post('/miles/hour', function() use ($app) {
+    $distance = new Kilometres($app->request->params('distance'));
+    $time = new Hours($app->request->params('time'));
+    $pace = new ImperialPace($distance, $time);
+
+    echo $distance->kilometres() . ' Km/Hour is ' . round($pace->milesHour(), 2) . ' Miles/Hour';
+});
+
+$app->post('/kilometres/hour', function() use ($app) {
+    $distance = new Miles($app->request->params('distance'));
+    $time = new Hours($app->request->params('time'));
+    $pace = new MetricPace($distance, $time);
+
+    echo $distance->miles() . ' Miles/Hour is ' . round($pace->kilometresHour(), 2) . ' Km/Hour';
 });
 
 
